0d565bfd939fd49dace90fe19f9225db0f57b9ea
keshavuluu <keshavulumuppa@example.com>
2025-04-20 00:10:55 +0530

    s changed

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
+import {
+  getMessages,
+  getUsersForSidebar,
+  sendMessage,
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
+// Specific routes should come before parameterized routes
 router.get("/users", protectRoute, getUsersForSidebar);
-router.get("/:id", protectRoute, getMessages);
-
 router.post("/send/:id", protectRoute, sendMessage);
+router.get("/:id", protectRoute, getMessages);
 
 export default router;
